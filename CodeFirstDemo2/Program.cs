7385db22018b78f3c086f7621eb8020b659c3895
Yiwen Zhao <crazyyiwen2015@gmail.com>
2019-05-21 22:54:45 -0400

    demo commit

---
+++ CodeFirstDemo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student()
+                {
+                    StudentName = "Bill"
+                };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
